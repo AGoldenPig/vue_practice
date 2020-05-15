8a62428cbab8ba748973f45f81c68c45af8f8f0b
Vincent <1164318685@qq.com>
2020-05-15 11:35:13 +0800

    add routers and components

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Book from '../views/Book.vue'
 
 Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name:'book',
+    component: Book
+  }
+]
 
 export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes : [
-  ]
+  routes : routes
 })
 
---
+++ src/views/Book.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>编号</td>
+                <td>书名</td>
+                <td>作者</td>
+            </tr>
+            <tr v-for="item in books">
+                <td>{{item.id}}</td>
+                <td>{{item.name}}</td>
+                <td>{{item.author}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Book",
+        data(){
+            return{
+                msg:"hello vue",
+                books: [
+                    {
+                        id: 1,
+                        name: 'html',
+                        author: '小明'
+                    },{
+                        id: 2,
+                        name: 'css',
+                        author: '小红'
+                    },{
+                        id: 3,
+                        name: 'js',
+                        author: '小亮'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
